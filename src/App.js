85ddbf8044419c6e8b7906c1141892e02a9a6776
Megan Adams <49360430+Meganimation@users.noreply.github.com>
2020-10-10 22:28:40 -0400

    added more calcs

--- src/App.js
+++ src/App.js
@@ -4,33 +4,78 @@ import './App.css';
 
 function App() {
 
+ 
+
   const [sharePrice, setSharePrice] = useState()
  
   const [numOfOutstandingShares, setNumOfOutstandingShares] = useState()
 
   const [newRevNum, setNewRevNum] = useState()
+  const [totalRevNum, setTotalRevNum] = useState()
+  const [unitRevNum, setUnitRevNum] = useState()
   const [prevYearNum, setPrevYearNum] = useState()
+  const [currentAssets, setCurrentAssets] = useState()
+  const [currentLiabilities, setCurrentLiabilites] = useState()
  
   const [state, setState] = useState({numOfShares: '2', anotherNum: '3'})
 
+
+
+
+
+  function checkRatio(num) {
+   if (num > 1) {
+     return 'apparently thats a good number'
+   }
+   else return ''
+  }
+
+
+
+
+
+  const currentRatio=()=>{
+    return (currentAssets && currentLiabilities) ? (currentAssets / currentLiabilities).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",") : '0'
+  }
 
   return (
     <div className="App">
       <header className="App-header">
       <br />   
-
-      <form> 
+<li>
+    
         Market Value = <input placeholder='shareprice' label='test' value={sharePrice} onChange={e => setSharePrice(e.target.value)} /> x <input placeholder='no of shares outstanding x 1000' value={numOfOutstandingShares} onChange={e => setNumOfOutstandingShares(e.target.value)} />
      <br/> 
       The Market Value is ${sharePrice && numOfOutstandingShares ? ((sharePrice * numOfOutstandingShares) * 1000).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",") : '0'} 
-      </form>
+   
 <br />
-      <form> 
-     <br />
+</li>
+<li>
+   
      Year-to-year percentage change = <input placeholder='new year rev num' value={newRevNum} onChange={e => setNewRevNum(e.target.value)} /> - <input placeholder='prev year num' value={prevYearNum} onChange={e => setPrevYearNum(e.target.value)} /> / <input value={prevYearNum} placeholder='prev year num' />
      <br />
      The y2y change is {newRevNum && prevYearNum ? (((newRevNum - prevYearNum) / prevYearNum) * 100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",") : '0'}%
-      </form>
+  
+      </li>
+    
+<li>
+   
+     how large a unit of a company is = <input placeholder='unit revenue' value={unitRevNum} onChange={e => setUnitRevNum(e.target.value)} /> - <input placeholder='total revenue' value={totalRevNum} onChange={e => setTotalRevNum(e.target.value)} /> x 100
+     <br />
+     1 company unit =  {unitRevNum && totalRevNum ? (unitRevNum / totalRevNum * 100).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",") : '0'}%
+  
+      </li>
+
+      <li>
+   
+     current ratio = <input placeholder='current assets' value={currentAssets} onChange={e => setCurrentAssets(e.target.value)} /> / <input placeholder='current liabilities' value={currentLiabilities} onChange={e => setCurrentLiabilites(e.target.value)} /> x 100
+     <br />
+     current ratio = {currentRatio()}
+    <small>
+     {checkRatio(currentAssets / currentLiabilities)}
+    </small>
+  
+      </li>
       </header>
     </div>
   );
